9cabbb4e7983590943d863f0e35239caa79e6466
Arthur <arthurrochadsg@gmail.com>
2023-09-16 12:17:59 -0300

    n

--- Hooks1/src/App.jsx
+++ Hooks1/src/App.jsx
@@ -1,34 +1,96 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import './cadastro.hook'
-import Checkbox from '@mui/material/Checkbox';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormGroup from '@mui/material/FormGroup';
+import { useState } from 'react'
 
-const label = { inputProps: { 'aria-label': 'Checkbox demo' } };
 function App() {
-
-  const [nome, setNome] = useState("")
- 
-  function handleEvt(e){
-    setNome(e.target.value)
-  }
-  
+const [name, setName] = useState("");
+const [genero, setGenero] = useState("");
+const [cpf, setCpf] = useState("")
+const [usuario, setUsuario] = useState("")
+const [dtnasc, setDtnasc] = useState("")
+const [senha, setSenha] = useState("")
+const [email, setEmail] = useState("")
+const [comfemail, setComfemail] = useState("")
+const [idiomacv, setIdiomaCv] = useState("")
+console.log(name)
   return (
-    <>
-      <div>
-       {nome}
-      </div>
-      <div className="card" id="card">
-      <input  type="text" onChange={handleEvt}  placeholder='Nome' />
-      <FormControlLabel control={<Checkbox defaultChecked />} label="Feminino" />
-      <FormControlLabel control={<Checkbox defaultChecked />} label="Masculino" />
-      </div>
+    <div className='App'>
+      <input id='nome' 
+      type='text' 
+      value={name} 
+      onChange={(e)=> setName(e.target.value)}
+      placeholder='Nome'>
+      </input>
       
-    </>
+      <input id='nome' 
+      type='text'
+      value={genero} 
+      onChange={(e)=> setGenero(e.target.value)}
+      placeholder='Gênero'>
+      </input>
+
+      <input id='nome' 
+      type='text'
+      value={cpf} 
+      onChange={(e)=> setCpf(e.target.value)}
+      placeholder='CPF'>
+      </input>
+
+      <input id='nome' 
+      type='text'
+      value={usuario} 
+      onChange={(e)=> setUsuario(e.target.value)}
+      placeholder='Usuario'>
+      </input>
+
+      <input id='nome' 
+      type='text'
+      value={dtnasc} 
+      onChange={(e)=> setDtnasc(e.target.value)}
+      placeholder='Data de Nascimento'>
+      </input>
+
+      <input id='nome' 
+      type='text'
+      value={senha} 
+      onChange={(e)=> setSenha(e.target.value)}
+      placeholder='Senha'>
+      </input>
+
+      <input id='nome' 
+      type='text'
+      value={email} 
+      onChange={(e)=> setEmail(e.target.value)}
+      placeholder='E-mail'>
+      </input>
+
+      <input id='nome' 
+      type='text'
+      value={comfemail} 
+      onChange={(e)=> setComfemail(e.target.value)}
+      placeholder='Confirme seu E-mail'>
+      </input>
+
+      <input id='nome' 
+      type='text'
+      value={idiomacv} 
+      onChange={(e)=> setIdiomaCv(e.target.value)}
+      placeholder='Idioma do curriculo'>
+      </input>
+      
+      <button>Cadastrar</button>
+
+      <h2>Meu nome é:{name}</h2>
+      <h2>{genero}</h2>
+      <h2>{cpf}</h2>
+      <h2>{usuario}</h2>
+      <h2>{dtnasc}</h2>
+      <h2>{senha}</h2>
+      <h2>{email}</h2>
+      <h2>{comfemail}</h2>
+      <h2>{idiomacv}</h2>
+     
+    </div>
   )
 }
-
 export default App
